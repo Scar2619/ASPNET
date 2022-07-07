d615d7dc77de86a6e61c1a1393292f55a4e9e9f1
Katelind Phillips <scarlettkate94@gmail.com>
2022-07-07 11:59:08 -0400

    delete product feature added

--- Testing/Controllers/ProductController.cs
+++ Testing/Controllers/ProductController.cs
@@ -62,6 +62,13 @@ namespace Testing.Controllers
             repo.InsertProduct(productToInsert);
             return RedirectToAction("Index");
         }
+
+        public IActionResult DeleteProduct(Product product)
+        {
+            repo.DeleteProduct(product);
+            return RedirectToAction("Index");
+        }
+
     }
 }
 
